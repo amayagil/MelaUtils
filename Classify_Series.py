99ab33782c1d8f531a556f012910115890e5c9a5
AmayaGil <amaya@redhat.com>
2016-11-29 00:37:02 +0100

    Para mover episodios que son directorios

--- Classify_Series.py
+++ Classify_Series.py
@@ -12,9 +12,9 @@ LOGFORMAT = "[%(asctime)s - %(levelname)s] %(message)s"
 
 nombre_serie = os.listdir(DIR)
 fobj = sys.stdout
-ftype_reg_exp = re.compile('.*\.(avi|mkv|mp4)$')
+ftype_reg_exp = re.compile('.*\.(avi|mkv|mp4|mpg|mpeg)$')
 formatter = logging.Formatter(LOGFORMAT)
-logger = logging.getLogger(__name__)
+logger = logging.getLogger()
 
 # file handler
 file = logging.FileHandler(LOGFILE)
@@ -32,31 +32,30 @@ try:
     fobj = open(LOGFILE, 'a')
 except Exception as e:
     if e.errno == 2:
-        print "No se pueden escribir logs, path no existe: " + e.strerror
+        print "No se pueden escribir logs, el path no existe: " + e.strerror
 
 for serie in nombre_serie:
     cwd = DIR + serie + '/'
     if os.path.isdir(cwd):
         episodios = os.listdir(cwd)
         for item in episodios:
-            logger.info('Started %s', cwd)
-            if os.path.isfile(cwd + item) and (ftype_reg_exp.match(cwd + item)):
+            if (os.path.isfile(cwd + item) and ftype_reg_exp.match(cwd + item)) or (os.path.isdir(cwd + item)):
                 try:
-                    temporada = re.findall("S|s[0-9]{2}", item)[0]
+                    temporada = re.findall("S[0-9]{2}|s[0-9]{2}", item)[0]
                 except IndexError:
-                    logger.warn("Formato de nombre de capitulo erroneo %s", cwd + item)
+                    logger.warn("Formato de nombre de capitulo erroneo %s" % (cwd + item))
                     continue
                 try:
                     if os.access(cwd, os.W_OK):
                         os.rename(cwd + item, cwd + temporada.upper() + '/' + item)
-                        logger.info("Episodio " + item + " movido a " + temporada.upper())
+                        logger.info("Episodio %s movido a %s" % (item, temporada.upper()))
                     else:
-                        logger.error("Error de permisos, imposible mover fichero %s", cwd + item)
+                        logger.error("Error de permisos, imposible mover fichero %s" % (cwd + item))
                 except OSError, e:
                     if e.errno == 2:
                         if os.access(cwd, os.W_OK):
                             os.mkdir(cwd + temporada.upper())
                             os.rename(cwd + item, cwd + temporada.upper() + '/' + item)
-                            logger.info("Episodio %s movido a %s, directorio %s creado", item, cwd + temporada.upper(), temporada.upper())
+                            logger.info("Episodio %s movido a %s, directorio %s creado" % (item, cwd + temporada.upper(), temporada.upper()))
                         else:
-                            logger.error("Error de permisos, imposible crear directorio %s", cwd+ temporada.upper())
+                            logger.error("Error de permisos, imposible crear directorio %s" % (cwd + temporada.upper()))
